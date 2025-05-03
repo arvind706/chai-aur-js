7c39eda120d2afa4b06473922a7ea036a18f6763
Webarm <professionalx78@gmail.com>
2025-05-03 16:03:39 +0000

    document config

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,14 @@
-console.log("Arvind")
+const accountId = 14452
+let accountEmail = "arvind@zeteapk.com"
+var accountPassword = "12345"
+accountCity = "Bengaluru"
+
+//accountId = 2; // not allowed
+
+accountEmail = "nothing@gmal.com"
+accountPassword = "64756"
+accountCity = "Bihar"
+
+// console.log(accoundId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
